95121d9523b53974ef0dd81d1c41edfd08ca48b4
wqd <wangqd@foxmail.com>
2024-12-31 23:00:25 +0800

    feat: basic parse of testB2412

--- src/gridreader/__init__.py
+++ src/gridreader/__init__.py
@@ -1,12 +1,14 @@
 from enum import Enum
-from .format import get_frames_with_seq, get_frames_with_header
-from kaitaistruct import KaitaiStruct
+from .format import get_frames_with_seq, get_frames_with_header, DataSet, frame_to_dict, check_frames
 from loguru import logger
 from .log import log_init
+from functools import partial
+import pandas as pd
 log_init()
 
 
-def parse_file(file_path:str, type: Enum):
+
+def parse_file(file_path:str, type: Enum, set:DataSet= DataSet.SCI):
     with open(file_path, "rb") as f:
         data = f.read()
     try:
@@ -14,4 +16,8 @@ def parse_file(file_path:str, type: Enum):
     except Exception as e:
         logger.warning(f"Failed to parse file {file_path} with {type} Seq, trying to parse with frame specified by header: {e}")
         frames = get_frames_with_header(data, type)
-    
+    frames_checked = check_frames(frames, type)
+    dicts = map(partial(frame_to_dict, type=type, set=set), frames_checked)
+
+    df = pd.DataFrame(dicts)
+    return df
--- src/gridreader/format/__init__.py
+++ src/gridreader/format/__init__.py
@@ -5,8 +5,17 @@ from . import wave_testB2412
 from enum import Enum
 from kaitaistruct import KaitaiStruct
 from ..log import log_init, logger
+from typing import Iterable, Callable, Any
 import re
+import crc
+
 log_init()
+
+
+class DataSet(Enum):
+    SCI = 0
+    RAW = 1
+    ALL = 2
 
 
 class SharedAutoEnum:
@@ -68,15 +77,30 @@ def get_header_length(type: Enum) -> tuple[int, int]:
             raise Exception("Not implemented")
 
 
-def get_frames_from_seq(seq: KaitaiStruct) -> list[KaitaiStruct]:
-    match type(seq):
+def get_frames_from_seq(seq: KaitaiStruct, type: Enum) -> list[KaitaiStruct]:
+    match type:
         case TestB2412.HK:
             return [f.frame for f in seq.frames]
-        case [TestB2412.FEAT, TestB2412.HK, TestB2412.TEL, TestB2412.WAVE]:
+        case TestB2412.FEAT | TestB2412.HK | TestB2412.TEL | TestB2412.WAVE:
             return seq.frames
         case _:
             raise Exception("Not implemented")
 
+
+crc16 = crc.Calculator(crc.Crc16.XMODEM, optimized=True)
+
+
+def get_checker(type: Enum) -> Callable[[KaitaiStruct], bool]:
+    match type:
+        case TestB2412.TEL:
+            return lambda f: f.sum == sum(f.body_data)
+        case TestB2412.FEAT | TestB2412.WAVE:
+            return lambda f: crc16.verify(bytes(f.body_data), f.crc)
+        case TestB2412.HK:
+            #TODO - HK data crc error
+            return lambda f: True
+        case _:
+            raise Exception("Not implemented")
 
 def get_frames_with_seq(bs: bytes, type: Enum) -> list[KaitaiStruct]:
     cls: KaitaiStruct = get_frameseq(type)
@@ -85,16 +109,17 @@ def get_frames_with_seq(bs: bytes, type: Enum) -> list[KaitaiStruct]:
     except Exception as e:
         logger.info(f"Parse with {type} Sequence Failed. Exception {e}, {e.args}")
         raise e
-    frames = get_frames_from_seq(seq)
+    frames = get_frames_from_seq(seq, type)
     return frames
 
 
 def get_frames_with_header(bs: bytes, type: Enum) -> list[KaitaiStruct]:
     cls: KaitaiStruct = get_frame(type)
     header, length = get_header_length(type)
-    pattern = re.compile(re.escape(header.to_bytes(len(header), "big")))
+    pattern = re.compile(re.escape(header.to_bytes(HEADER_LENGTH, "big")))
     matches = [m.start() for m in pattern.finditer(bs)]
     frames = []
+    fail_count = 0
     for match in matches:
         if match + length > len(bs):
             break
@@ -102,5 +127,52 @@ def get_frames_with_header(bs: bytes, type: Enum) -> list[KaitaiStruct]:
         try:
             frames.append(cls.from_bytes(data))
         except Exception as e:
-            logger.debug(f"Parse {data} with {type} Header Failed. Exception {e}, {e.args}")
+            fail_count += 1
+            logger.debug(
+                f"Parse {data} with {type} Header Failed. Exception {e}, {e.args}"
+            )
+    logger.info(
+        f"Parse {type} with Header Failed {fail_count} times, got {len(frames)} frames"
+    )
     return frames
+
+
+def check_frames(frames: list[KaitaiStruct], type: Enum) -> Iterable[KaitaiStruct]:
+    checker = get_checker(type)
+    for frame in frames:
+        if checker(frame):
+            yield frame
+
+
+def frame_to_sci(frame: dict[str, Any], type: Enum) -> dict[str, Any]:
+    match type:
+        case TestB2412.TEL:
+            return {
+                f"sipm_temp{i}inC": frame[f"sipm_temperature_ch{i}"] / 100 - 273.15
+                for i in range(4)
+            }
+        case TestB2412.HK:
+            temps = {
+                "sipm_temp{i}inC": frame[f"temperature_sipm_ch{i}"] / 100 - 273.15
+                for i in range(4)
+            }
+            bias = {
+                "sipm_bias{i}inV": frame[f"voltage_sipm_ch{i}"] * 1e-3 for i in range(4)
+            }
+            temps.update(bias)
+            return temps
+        case _:
+            return {}
+
+
+def frame_to_dict(frame: KaitaiStruct, type: Enum, set: DataSet) -> dict[str, Any]:
+    raw = {k: getattr(frame, k) for k in frame.__dict__ if not k.startswith("_")}
+    if set == DataSet.RAW:
+        return raw
+    sci = frame_to_sci(raw, type)
+    if set == DataSet.SCI:
+        return sci
+    elif set == DataSet.ALL:
+        return {**raw, **sci}
+    else:
+        raise Exception("Not implemented")
